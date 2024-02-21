859959678d1a0832e381bdc50f565f74f7b29328
jai <jaisinghdang64@gmail.com>
2024-02-22 00:35:17 +0530

    changes

---
+++ pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductHomePage from '../Components/HomeComponent/HomeComponent'
+
+function Home() {
+  return (
+    <div><ProductHomePage/></div>
+  )
+}
+
+export default Home
